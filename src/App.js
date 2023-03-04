e4ab54c6ddea784db0786cb96dd6546c9e4deff5
BharAni <bharaniarasu.t@gmail.com>
2023-03-04 07:59:45 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Cart from "./cart/cart";
 import Header from "./layout/header";
 import Meals from "./meals/meals";
 import "./styles.css";
+import CartProvider from "./store/cart_provider";
 
 export default function App() {
   const [cartShown, setCartShown] = useState(false);
@@ -15,12 +16,12 @@ export default function App() {
     setCartShown(false);
   };
   return (
-    <>
+    <CartProvider>
       {cartShown && <Cart onHideCart={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
